0a16123f4466292d0f4b1f04b95cc31337bb97f4
dmalia1 <althor@k-state.edu>
2019-08-02 06:55:29 -0500

    Changed hard coded path to match home.

--- src/awscli_login/windaemon.py
+++ src/awscli_login/windaemon.py
@@ -6,22 +6,22 @@ from daemoniker import Daemonizer
 
 def main():
     dummy = "Hello World"
-    pidFile = os.path.join("C:\\Users\\althor.FC-OME-JEFFERSO", "test.pid")
-    testFile = os.path.join("C:\\Users\\althor.FC-OME-JEFFERSO", "pyTest.txt")
+    pidFile = os.path.join("C:\\Users\\althor", "test.pid")
+    testFile = os.path.join("C:\\Users\\althor", "pyTest.txt")
 
 
 if __name__ == '__main__':
     dummy = "Hello World"
     with Daemonizer() as (is_setup, daemonizer):
         is_parent, dummy = daemonizer(
-            os.path.join("C:\\Users\\althor.FC-OME-JEFFERSO", "test.pid"), dummy
+            os.path.join("C:\\Users\\althor", "test.pid"), dummy
         )
         if is_parent:
             print("in parent")
             dummy = "Hello Parent"
         if not is_parent:
             print("in child")
-            testFile = os.path.join("C:\\Users\\althor.FC-OME-JEFFERSO", "pyTest.txt")
+            testFile = os.path.join("C:\\Users\\althor", "pyTest.txt")
             f = open(testFile, 'a')
             f.write(dummy)
             f.close()
