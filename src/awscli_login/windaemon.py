d47da754ed438e4bc81ff50aedc3a625f2c08bec
dmalia1 <althor@k-state.edu>
2019-08-05 15:17:08 -0500

    more cleanup

--- src/awscli_login/windaemon.py
+++ src/awscli_login/windaemon.py
@@ -20,33 +20,51 @@ def lookup_args():
     args = None
     argpath = sys.argv[1]
     if os.path.exists(argpath):
-        with open(argpath, 'rb') as f:
-            args = pickle.load(f)
-        argFile = tempfile.NamedTemporaryFile()
-        tempArgPath = argFile.name + "A"
-        os.environ["__AWS_CLI_WINDAEMON__"] = tempArgPath
-        if not os.path.exists(os.environ["__AWS_CLI_WINDAEMON__"]):
-            with open(tempArgPath, 'wb') as fw:
-                # Use the highest available protocol
-                pickle.dump(args, fw, protocol=-1)
-        os.environ["__AWS_CLI_PID__"] = args[0].pidfile
+        args = load_args_for_parent(argpath, args)
         return args
     elif os.path.exists(os.environ["__AWS_CLI_WINDAEMON__"]):
         try:
-            with open(os.environ["__AWS_CLI_WINDAEMON__"], 'rb') as f:
-                args = pickle.load(f)
-            with open(os.environ["__AWS_CLI_WINDAEMON__"], 'r+b') as f:
-                to_erase = f.read()
-                eraser = bytes(len(to_erase))
-                f.seek(0)
-                f.write(eraser)
-            os.remove(os.environ["__AWS_CLI_WINDAEMON__"])
-            del os.environ["__AWS_CLI_WINDAEMON__"]
+            args = load_args_for_child(args)
             return args
         except Exception as exc:
             raise Exception(
                 'Problem loading args from file'
             ) from exc
+
+
+def load_args_for_child(args):
+    with open(os.environ["__AWS_CLI_WINDAEMON__"], 'rb') as f:
+        args = pickle.load(f)
+    cleanup_tmp_file()
+    return args
+
+
+def load_args_for_parent(argpath, args):
+    with open(argpath, 'rb') as f:
+        args = pickle.load(f)
+    store_args_for_child(args)
+    return args
+
+
+def store_args_for_child(args):
+    argFile = tempfile.NamedTemporaryFile()
+    tempArgPath = argFile.name + "A"
+    os.environ["__AWS_CLI_WINDAEMON__"] = tempArgPath
+
+    if not os.path.exists(os.environ["__AWS_CLI_WINDAEMON__"]):
+        with open(tempArgPath, 'wb') as fw:
+            # Use the highest available protocol
+            pickle.dump(args, fw, protocol=-1)
+
+
+def cleanup_tmp_file():
+    with open(os.environ["__AWS_CLI_WINDAEMON__"], 'r+b') as f:
+        to_erase = f.read()
+        eraser = bytes(len(to_erase))
+        f.seek(0)
+        f.write(eraser)
+    os.remove(os.environ["__AWS_CLI_WINDAEMON__"])
+    del os.environ["__AWS_CLI_WINDAEMON__"]
 
 
 def daemonize_for_windows(profile, role, expires):
