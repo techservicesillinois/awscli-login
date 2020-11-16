c45f2a318e2dc5dac19243d2066d47ab89a2adc5
David D. Riddle <ddriddle@illinois.edu>
2020-11-16 17:51:04 -0600

    Add automated testing and release using Github
    - Makefile refactor
        - Adds cache target to support Github Actions caching
        - Adds fine-grained deps targets to Makefile
        - Adds a develop target for running unit tests
        - Adds release targets for pushing code to PyPI
        - The test target now uses tox to run unit tests
        - See development guide for more details
    - Github Actions
        - The CI/CD pipeline is run on every push
        - The build stage creates a wheel
        - All tests are run on push against the wheel created from the
          build stage
        - Tests run on Linux, Mac OS X, and Windows
        - Tests run on Python 3.5, 3.6, 3.7, 3.8, and 3.8
        - All tests use tox
        - Tox virtual environment(s) are cached
            - Cache is deleted if more than a week old
        - After the tests pass, code pushed to master is automatically
          released to Test PyPI
        - After the tests pass, tagged code is automatically released
          to PyPI

--- README.rst
+++ README.rst
@@ -1,3 +1,7 @@
+.. image:: https://github.com/techservicesillinois/awscli-login/workflows/CI/CD/badge.svg
+   :target: https://github.com/techservicesillinois/awscli-login/actions?query=workflow%3ACI%2FCD
+   :alt: Build Status
+
 The awscli-login plugin allows retrieving temporary Amazon credentials by
 authenticating against a SAML Identity Provider (IdP).
 This application is supported under Linux, MacOS, and the `Windows Subsystem for Linux
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
-from os import path
+from os import path, environ
+from warnings import warn
 
 here = path.abspath(path.dirname(__file__))
 
@@ -8,9 +9,91 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+
+def version():
+    """Returns a valid PEP 440 version string, or an invalid version
+    string on error. """
+    if "GITHUB_ACTIONS" in environ:
+        return github_release()
+    else:
+        return local_release()
+
+
+def test_version(sha):
+    """Returns a valid PEP 440 version string for use with CI builds.
+    """
+    from datetime import datetime
+
+    now = datetime.now()
+    return str(now.year) + \
+        '.' + str(now.month) + \
+        '.' + str(now.day) + \
+        '.' + str(now.hour) + \
+        '.' + str(now.minute) + \
+        '.' + str(now.second) + \
+        '.dev' + format(int(sha[0:7], 16), 'o')  # convert short sha to octal
+
+
+def github_release():
+    """Returns a git tag for tagged releases, or an automatically
+    generated test release number otherwise. """
+    # Production release
+    tag = environ.get('GITHUB_REF', '')
+    if tag.startswith('refs/tags/'):
+        return tag.lstrip('refs/tags/')
+
+    # Test release
+    return test_version(environ['GITHUB_SHA'])
+
+
+def find_tags(repo, sha):
+    """Returns a list of tags that point to the given commit sha.
+    """
+    tags = []
+
+    for tag in repo.tags:
+        if tag.object.hexsha == sha:
+            tags.append(tag.name)
+
+    return tags
+
+
+def local_release():
+    """If everything is commited in the Git stage then this function
+    returns the git tag for the current commit if it exists, or a
+    automatically generated version based on the date & time for
+    CI builds, otherwise an invalid PEP 440 version string is
+    returned to prevent uploads to PyPI. """
+
+    # This string is an invalid version per PEP 440, and should be
+    # rejected by production and test PyPI
+    BAD_RELEASE = 'invalid.version'
+
+    try:
+        import git
+    except ImportError:
+        return BAD_RELEASE
+
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha
+
+    if repo.is_dirty():
+        warn("Please commit your code before building!")
+        return BAD_RELEASE
+
+    tags = find_tags(repo, sha)
+    if len(tags) == 1:  # Production release
+        return tags[0]
+    elif len(tags) > 1:
+        warn("Commit has more than one tag: %s" % tags)
+        return BAD_RELEASE
+    else:  # Test release
+        return test_version(repo.head.object.hexsha)
+
+
 setup(
     name='awscli-login',
-    version='0.1.0a6',  # TODO change this to a git tag for Drone
+    version=version(),
     description='Plugin for the AWS CLI that retrieves and rotates '
     'credentials using SAML ECP and STS.',
     long_description=long_description,
@@ -46,13 +129,7 @@ setup(
     ],
     extras_require={
         'test': [
-            'Sphinx',
-            'coverage',
-            'sphinx-autodoc-typehints',
-            'sphinx_rtd_theme',
             'tblib',
-            'tox',
-            'tox-pyenv',
             'wurlitzer',
         ],
     },
