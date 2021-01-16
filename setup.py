2334c58faa460bee709a51adf85207debd57c1f5
David D. Riddle <ddriddle@illinois.edu>
2021-01-16 13:12:06 -0600

    Use setuptools_scm for version management
    This commit drops the custom code used to calculate versions on
    build, and instead uses setuptools_scm.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 
-from os import path, environ
-from warnings import warn
+from os import path
 
 here = path.abspath(path.dirname(__file__))
 
@@ -11,89 +10,20 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 
 def version():
-    """Returns a valid PEP 440 version string, or an invalid version
-    string on error. """
-    if "GITHUB_ACTIONS" in environ:
-        return github_release()
-    else:
-        return local_release()
+    from setuptools_scm.version import get_local_dirty_tag
 
+    def clean_scheme(version):
+        # If code is not commited add +dirty to version to prevent
+        # upload to either PyPY or test PyPY.
+        return get_local_dirty_tag(version) if version.dirty else ''
 
-def test_version(sha):
-    """Returns a valid PEP 440 version string for use with CI builds.
-    """
-    from datetime import datetime
-
-    now = datetime.now()
-    return str(now.year) + \
-        '.' + str(now.month) + \
-        '.' + str(now.day) + \
-        '.' + str(now.hour) + \
-        '.' + str(now.minute) + \
-        '.' + str(now.second) + \
-        '.dev' + format(int(sha[0:7], 16), 'o')  # convert short sha to octal
-
-
-def github_release():
-    """Returns a git tag for tagged releases, or an automatically
-    generated test release number otherwise. """
-    # Production release
-    tag = environ.get('GITHUB_REF', '')
-    if tag.startswith('refs/tags/'):
-        return tag.lstrip('refs/tags/')
-
-    # Test release
-    return test_version(environ['GITHUB_SHA'])
-
-
-def find_tags(repo, sha):
-    """Returns a list of tags that point to the given commit sha.
-    """
-    tags = []
-
-    for tag in repo.tags:
-        if tag.object.hexsha == sha:
-            tags.append(tag.name)
-
-    return tags
-
-
-def local_release():
-    """If everything is commited in the Git stage then this function
-    returns the git tag for the current commit if it exists, or a
-    automatically generated version based on the date & time for
-    CI builds, otherwise an invalid PEP 440 version string is
-    returned to prevent uploads to PyPI. """
-
-    # This string is an invalid version per PEP 440, and should be
-    # rejected by production and test PyPI
-    BAD_RELEASE = 'invalid.version'
-
-    try:
-        import git
-    except ImportError:
-        return BAD_RELEASE
-
-    repo = git.Repo(search_parent_directories=True)
-    sha = repo.head.object.hexsha
-
-    if repo.is_dirty():
-        warn("Please commit your code before building!")
-        return BAD_RELEASE
-
-    tags = find_tags(repo, sha)
-    if len(tags) == 1:  # Production release
-        return tags[0]
-    elif len(tags) > 1:
-        warn("Commit has more than one tag: %s" % tags)
-        return BAD_RELEASE
-    else:  # Test release
-        return test_version(repo.head.object.hexsha)
+    return {'local_scheme': clean_scheme}
 
 
 setup(
     name='awscli-login',
-    version=version(),
+    use_scm_version=version,
+    setup_requires=['setuptools_scm'],
     description='Plugin for the AWS CLI that retrieves and rotates '
     'credentials using SAML ECP and STS.',
     long_description=long_description,
