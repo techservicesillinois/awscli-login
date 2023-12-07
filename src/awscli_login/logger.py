a71bc4dea20bcbdd163cbe1e0b8f5f90e1a5c12e
David D. Riddle <ddriddle@illinois.edu>
2023-12-07 13:27:48 -0600

    Fix broken build
    Unit tests fail since awscli is throwing SyntaxWarnings. This commit
    turns down the logging level for messages coming from awscli.
    
    Closes #173

--- src/awscli_login/logger.py
+++ src/awscli_login/logger.py
@@ -7,11 +7,16 @@ def _cli_options(verbosity: int) -> Tuple[int, str]:
     fmt = '%(message)s'
     level = logging.WARN
 
+    # Suppress awscli syntax warnings #173
+    awscli = logging.getLogger("awscli")
+    awscli.setLevel(logging.ERROR)
+
     if verbosity == 1:
         level = logging.INFO
 
         urllib3 = logging.getLogger("urllib3")
         urllib3.setLevel(logging.DEBUG)
+        awscli.setLevel(logging.WARN)
 
     if verbosity == 2:
         level = logging.DEBUG
@@ -21,6 +26,7 @@ def _cli_options(verbosity: int) -> Tuple[int, str]:
 
         botocore = logging.getLogger("botocore.endpoint")
         botocore.setLevel(logging.DEBUG)
+        awscli.setLevel(logging.INFO)
 
     if verbosity == 3:
         fmt = '%(name)s %(message)s'
