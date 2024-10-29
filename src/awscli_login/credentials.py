3ba1e1f9318f04d167d6eb439ff6c9fb499c41e4
David D. Riddle <ddriddle@users.noreply.github.com>
2024-10-29 14:14:22 -0500

    Fix C library conflicts (#223)
    Closes #222
    Closes #230

--- src/awscli_login/credentials.py
+++ src/awscli_login/credentials.py
@@ -2,6 +2,9 @@
 
 import argparse
 import json
+import os
+import platform
+import sys
 
 from argparse import Namespace
 from datetime import datetime
@@ -10,6 +13,7 @@ from botocore.session import Session
 
 from .__main__ import main as login, logout
 from .config import Profile, error_handler
+from ._version import version
 
 
 def print_credentials(token):
@@ -45,6 +49,11 @@ def init_parser():
         action="count",
         default=0,
         help="Display verbose information")
+    parser.add_argument(
+        "-d",
+        "--debug-info",
+        action='store_true',
+        help="Display debug information")
 
     hidden = parser.add_mutually_exclusive_group()
     for flag in ["--login", "--logout"]:
@@ -63,13 +72,44 @@ def _main(profile: Profile, session: Session, interactive: bool = True):
     print_credentials(token)
 
 
+def debug_info():
+    executable = sys.executable if platform.system() != "Windows" else \
+        sys.executable.lower()
+    nl = "\n"  # Workaround for versions before 3.12 (See PEP 701)
+    info = f"Version: {version}\n" f"Python executable: {executable}\n" \
+           f"Python Version: {platform.python_version()}\n" \
+           f"Python Compiler: {platform.python_compiler()}\n" \
+           f"Python Implementation: {sys.implementation}\n" \
+           f"Python Path:\n{nl.join(sys.path)}\n" \
+           f"System Path:\n{nl.join(sys.path)}\n"
+
+    def env(func):
+        ignore = ["AWS_DATA_PATH", "SHLVL", "_"]
+
+        return "Environment:\n" + '\n'.join(sorted([
+            f"{k}: {func(v)}" for k, v in os.environ.items()
+            if k not in ignore
+        ]))
+
+    if platform.system() == 'Windows':
+        environment = env(lambda x: x.lower().strip())
+    else:
+        environment = env(lambda x: x)
+    print(info + environment)
+
+
 def main():
     # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-sourcing-external.html
     args = init_parser().parse_args()
     session = Session(profile=args.profile)
-
-    if args.login:
-        return login(Namespace(**json.load(args.login)), session)
+    if args.debug_info:
+        debug_info()
+    elif args.login:
+        ns = Namespace(**json.load(args.login))
+        if ns.debug_info:
+            debug_info()
+            return
+        return login(ns, session)
     elif args.logout:
         return logout(Namespace(**json.load(args.logout)), session)
     else:
