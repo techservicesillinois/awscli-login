9d44089b5d902c28c1a1a6ae70bdc864ad530ee4
David D. Riddle <ddriddle@users.noreply.github.com>
2025-02-21 16:49:11 -0600

    Add support for Python 3.13 (#219)
    * Add better error messages to raise_if_credential_process_not_set
    * Drop support for Python 3.8
    
    Close #218
    Close #225
    Close #233
    Close #234

--- src/awscli_login/const.py
+++ src/awscli_login/const.py
@@ -7,3 +7,10 @@ OVERWRITE_PROFILE = "Overwrite profile '%s' to enable login? "
 
 DUO_HEADER_FACTOR = 'X-Shibboleth-Duo-Factor'
 DUO_HEADER_PASSCODE = 'X-Shibboleth-Duo-Passcode'
+
+ERROR_INVALID_CRED_PROC_PATH = \
+    "credential_process:%s: Is not on $PATH or not executable."
+ERROR_INVALID_CRED_PROC_WRONG_PROFILE_ARG = \
+    "credential_process:%s: --profile not set to current profile '%s'."
+ERROR_INVALID_CRED_PROC_MISSING_PROFILE_ARG = \
+    "credential_process:%s: --profile argument not set."
